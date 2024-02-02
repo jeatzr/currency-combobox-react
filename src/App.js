bfb537064a3fe6a0f016ea90f0a24e173df4063a
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2024-02-02 13:47:45 +0100

    Flags

--- src/App.js
+++ src/App.js
@@ -5,79 +5,94 @@ const currencies = {
   "USD": {
     "emoji": "\uD83C\uDDFA\uD83C\uDDF8",
     "exchangeRate": 1,
-    "name": "US Dollar"
+    "name": "US Dollar",
+    "flag": "us.png"
   },
   "EUR": {
     "emoji": "\uD83C\uDDEA\uD83C\uDDFA",
     "exchangeRate": 0.89,
-    "name": "Euro"
+    "name": "Euro",
+    "flag": "eu.png"
   },
   "JPY": {
     "emoji": "\uD83C\uDDEF\uD83C\uDDF5",
     "exchangeRate": 114.42,
-    "name": "Japanese Yen"
+    "name": "Japanese Yen",
+    "flag": "jp.png"
   },
   "GBP": {
     "emoji": "\uD83C\uDDEC\uD83C\uDDE7",
     "exchangeRate": 0.75,
-    "name": "British Pound"
+    "name": "British Pound",
+    "flag": "gb.png"
   },
   "AUD": {
     "emoji": "\uD83C\uDDE6\uD83C\uDDFA",
     "exchangeRate": 1.35,
-    "name": "Australian Dollar"
+    "name": "Australian Dollar",
+    "flag": "au.png"
   },
   "CAD": {
     "emoji": "\uD83C\uDDE8\uD83C\uDDE6",
     "exchangeRate": 1.28,
-    "name": "Canadian Dollar"
+    "name": "Canadian Dollar",
+    "flag": "ca.png"
   },
   "CHF": {
     "emoji": "\uD83C\uDDE8\uD83C\uDDED",
     "exchangeRate": 0.93,
-    "name": "Swiss Franc"
+    "name": "Swiss Franc",
+    "flag": "ch.png"
   },
   "CNY": {
     "emoji": "\uD83C\uDDE8\uD83C\uDDF3",
     "exchangeRate": 6.36,
-    "name": "Chinese Yuan"
+    "name": "Chinese Yuan",
+    "flag": "cn.png"
   },
   "SEK": {
     "emoji": "\uD83C\uDDF8\uD83C\uDDEA",
     "exchangeRate": 8.51,
-    "name": "Swedish Krona"
+    "name": "Swedish Krona",
+    "flag": "se.png"
   },
   "NZD": {
     "emoji": "\uD83C\uDDF3\uD83C\uDDFF",
     "exchangeRate": 1.49,
-    "name": "New Zealand Dollar"
+    "name": "New Zealand Dollar",
+    "flag": "nz.png"
   },
   "INR": {
     "emoji": "\uD83C\uDDEE\uD83C\uDDF3",
     "exchangeRate": 74.57,
-    "name": "Indian Rupee"
+    "name": "Indian Rupee",
+    "flag": "in.png"
   },
   "BRL": {
     "emoji": "\uD83C\uDDE7\uD83C\uDDF7",
     "exchangeRate": 5.22,
-    "name": "Brazilian Real"
+    "name": "Brazilian Real",
+    "flag": "br.png"
   },
   "RUB": {
     "emoji": "\uD83C\uDDF7\uD83C\uDDFA",
     "exchangeRate": 73.96,
-    "name": "Russian Ruble"
+    "name": "Russian Ruble",
+    "flag": "ru.png"
   },
   "ZAR": {
     "emoji": "\uD83C\uDDFF\uD83C\uDDE6",
     "exchangeRate": 16.96,
-    "name": "South African Rand"
+    "name": "South African Rand",
+    "flag": "za.png"
   },
   "MXN": {
     "emoji": "\uD83C\uDDF2\uD83C\uDDFD",
     "exchangeRate": 20.45,
-    "name": "Mexican Peso"
+    "name": "Mexican Peso",
+    "flag": "mx.png"
   }
-  // Puedes agregar más códigos de moneda, emojis de banderas y nombres de moneda según tus necesidades
+  // Puedes agregar más códigos de moneda, emojis de banderas, nombres de moneda y nombres de archivos de banderas según tus necesidades
 }
 const initialExchanges = [
   {
@@ -106,6 +121,12 @@ const App = () => {
   return (
     <div>
       <h1>Selected Currency: {selectedCurrency}</h1>
+      {selectedCurrency &&
+        <img
+          alt={selectedCurrency}
+          src={"/flags/" + currencies[selectedCurrency].flag}>
+        </img>
+      }
       <CurrencyComboBox currencies={currencies} onSelectCurrency={handleSelectCurrency} />
     </div>
   );
